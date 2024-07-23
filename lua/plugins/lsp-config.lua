777f0794b4ecd138a5e425d98b4c5134e21ca3cb
Volteal <wendy.a.knight@icloud.com>
2024-07-23 18:56:14 +0200

    I set up LSPs for: Lua, golang, Python, and PHP.

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "golangci_lint_ls",
+          "pyright",
+          "intelephense"
+        },
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require('lspconfig')
+      lspconfig.lua_ls.setup({})
+      lspconfig.golangci_lint_ls.setup({})
+      lspconfig.pyright.setup({})
+      lspconfig.intelephense.setup({})
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  }
+}

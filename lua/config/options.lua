31711a7d7c9d72627bc585deb88ce309a20ab6a6
volteal <volteal@ares-echo.local>
2025-02-15 14:58:57 +0100

    finished config.

---
+++ lua/config/options.lua
@@ -0,0 +1,11 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+vim.cmd("set numberwidth=3")
+
+vim.g.mapleader = " "
+
+vim.opt.spell = true
+vim.opt.spelllang = "en_us"

ff1346b2250076f8e309f8b99b6d77bd0017ff7d
volteal <volteal@ares-echo.local>
2025-03-16 21:04:01 +0100

    added toggle term to work with lazygit.

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,22 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = "*",
+  config = function()
+    require("toggleterm").setup{}
+
+    local Terminal  = require('toggleterm.terminal').Terminal
+    local lazygit = Terminal:new({
+      cmd = "lazygit",
+      dir = "git_dir",
+      direction = "float",
+      float_opts = {
+        border = "double",
+      },
+      hidden = true
+    })
+    function _lazygit_toggle()
+      lazygit:toggle()
+    end
+    vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+  end
+}
